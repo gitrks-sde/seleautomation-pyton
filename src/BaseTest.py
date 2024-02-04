0f260c753059858c63705705cffdc8a807724f21
git-sde-rks <karthi50rks@gmail.com>
2024-02-04 11:54:48 +0530

    Basetest update

--- src/BaseTest.py
+++ src/BaseTest.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.by import By
 
 driver=webdriver.Chrome()
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 driver.maximize_window()
+input=driver.find_element(By.ID,"my-text-id")
+input.send_keys("Tutorial");
 time.sleep(5)
